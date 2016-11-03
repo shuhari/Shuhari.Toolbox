a99b1a736157e75aa1b6089d773a6929b8c605f4
shuhari <shuhari@outlook.com>
2016-11-03 13:42:35 +0800

    Remove old files

--- src/main.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from app import App
-
-
-if __name__ == '__main__':
-    app = App()
-    app.run()
--- src/mainwindow.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys
-from PyQt5.QtWidgets import *
-
-
-class MainWindow(QWidget):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.setWindowTitle("Shuhari's Toolbox")
